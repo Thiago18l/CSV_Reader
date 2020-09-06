00fca44880cfb127d18451446a9aa06940773a12
Thiago Lopes <thiago.lopes.dev@gmail.com>
2020-09-06 19:10:59 -0300

    feat: add man united won games count

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,12 @@ const matches = fs.readFileSync("football.csv", {
 });
 
 
-
-
-console.log(matches);
+let manUnitedWins = 0;
+for (let match of matches) {
+    if (match[1] === "Man United" && match[5] === "H") {
+        manUnitedWins++;
+    } else if (match[2] === "Man United" && match[5] === "A") {
+        manUnitedWins++;
+    }
+}
+console.log(manUnitedWins);
