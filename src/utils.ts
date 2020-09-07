2fc8e7bedba17860cfae3e86093ea862f41fe98e
Thiago Lopes <thiago.lopes.dev@gmail.com>
2020-09-07 01:23:13 -0300

    feat: move MatchResults to a isolated file

---
+++ src/utils.ts
@@ -0,0 +1,9 @@
+export const dateStringToDate = (rowDate: string): Date => {
+    const date = rowDate
+    .split("/")
+    .map((value: string): number => {
+        return parseInt(value);
+    });
+
+    return new Date(date[2], date[1] - 1, date[0]);
+};
