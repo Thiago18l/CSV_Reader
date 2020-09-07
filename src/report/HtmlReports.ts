504404e4744cd5695c45d762085f2b56a7313f22
Thiago Lopes <thiago.lopes.dev@gmail.com>
2020-09-07 16:55:46 -0300

    feat: add HtmlReports to generate reports writing a new file

---
+++ src/report/HtmlReports.ts
@@ -0,0 +1,14 @@
+import { IOutputTarget } from '../Summary';
+import fs from 'fs'
+export class HtmlReports implements IOutputTarget {
+
+    print(report: string): void {
+        const html = `
+        <div>
+            <h1>Analysis Output</h1>
+            <div>${report}</div>
+        </div>
+        `;
+        fs.writeFileSync('report.html', html);
+    }
+}
