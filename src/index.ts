272a84f3d633f2605883c297a2ac8970a1684d27
Thiago Lopes <thiago.lopes.dev@gmail.com>
2020-09-07 16:56:07 -0300

    chore: finish the app

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { MatchReader } from './MatchReader';
 import { CsvFileReader } from './CsvFileReader';
 import { ConsoleReports } from './report/ConsoleReports';
+import { HtmlReports } from './report/HtmlReports';
 import { WinsAnalysis } from './analyzers/WinsAnalysis';
 import { Summary } from './Summary';
 
@@ -9,6 +10,6 @@ const csv_Reader = new CsvFileReader("football.csv");
 const match_Reader = new MatchReader(csv_Reader);
 match_Reader.load();
 
-const summary = new Summary(new WinsAnalysis('Man United'), new ConsoleReports());
+const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReports());
 
 summary.buildAndPrintReport(match_Reader.matches);
