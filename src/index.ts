f02145ead401fa53e59d087af8d17267ed4a06f7
Thiago Lopes <thiago.lopes.dev@gmail.com>
2020-09-07 00:51:49 -0300

    feat: import CsvFileReader

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,21 @@
-import fs from 'fs';
+import { CsvFileReader } from './CsvFileReader';
 
+const reader = new CsvFileReader("football.csv");
+reader.read();
 
-const matches = fs.readFileSync("football.csv", {
-    encoding: 'utf-8',
-}).split("\n")
-.map((row: string): string[] => {
-    return row.split(",");
-});
+enum MatchResult {
+  homeWin = 'H',
+  awayWin = 'A',
+  draw = 'D'  
+};
 
 
 let manUnitedWins = 0;
-for (let match of matches) {
-    if (match[1] === "Man United" && match[5] === "H") {
+for (let match of reader.data) {
+    if (match[1] === "Man United" && match[5] === MatchResult.homeWin) {
         manUnitedWins++;
-    } else if (match[2] === "Man United" && match[5] === "A") {
+    } else if (match[2] === "Man United" && match[5] === MatchResult.awayWin) {
         manUnitedWins++;
     }
 }
-console.log(manUnitedWins);
+console.log(`Man United won ${manUnitedWins} games`);
